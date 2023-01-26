5ef989327f6022415d4f182d3931c0e50d8d5499
kevinksit <kevinkamansit@gmail.com>
2023-01-26 09:47:38 -0800

    udpated

--- Cell.py
+++ Cell.py
@@ -1,13 +1,15 @@
 import pandas as pd
 import numpy as np
-
+import itertools
 
 class Cell:
-    def __init__(self, neural_data:np.ndarray, s2p_data:dict):
+    def __init__(self, neural_data:np.ndarray, s2p_data:dict, is_cell:bool, frame_rate:int):
         self.s2p_data = s2p_data # this should be a dict, from s2p's stat.npy
         self.neural_data = neural_data
+        self.is_cell = is_cell
         self.labels = pd.DataFrame()
         self.outputs = pd.DataFrame()
+        self.time = np.arange(neural_data.shape[0])/frame_rate
 
     def add_label(self, input, label_name=None):
         input, name = self._check_inputs(input, label_name)
@@ -28,21 +30,32 @@ class Cell:
             raise IndexError(f"Labels do not have the right number of trials ({self.neural_data.shape[-1]})") # trials
         return input, name
 
-    def average_over(self, input):
-        # check if oculmn exists
-        # if exists, group and return mean by that column of trialwise data right?
-        if input is list:
-            raise Exception(f"Multi-averaging not implemented yet... sorry")
-        if input not in self.labels.columns:
-            raise Exception(f"{input} does not exist in labels ({self.labels.columns})")
-        target = self.labels[input]
-        u_targets = sorted(target.unique())
-        avg_response = np.zeros((1, len(u_targets)))
-        for ct, u in enumerate(u_targets):
-            idx = (target.to_numpy() == u)
-            avg_response[:, ct] = np.mean(self.neural_data[:, idx])
-        return np.squeeze(avg_response), u_targets
+    def average_over(self, win, input=None):
+        baseline_idx = (self.time > win[0]) & (self.time < win[1])
+        response_idx = (self.time > win[2]) & (self.time < win[3])
+        base = self.neural_data[baseline_idx, :]
+        resp = self.neural_data[response_idx, :]
+        if input is None: # grand average across conditions
+            return np.mean(resp) - np.mean(base), np.arange(self.neural_data.shape[-1])
 
-        
+        # check if input is a list
+        if type(input) is not list:
+            input = [input]
 
-
+        targets = list()
+        u_targets = list()
+        for i in input:
+            # for all the inputs, get the proper values
+            targets.append(self.labels[i])
+            u_targets.append(sorted(self.labels[i].unique()))
+        subtracted_response = np.zeros([len(x) for x in u_targets])
+        # not the fastest way of doing this... can we think of something faster? idk
+        for lin_idx, combo in enumerate(itertools.product(*u_targets)):
+            # for each potential combination of targets (list product)
+            select = np.zeros((len(targets), self.neural_data.shape[-1]))  # this is targets x trials
+            for idx, val in enumerate(combo):
+                # for each target, collect the trues for that value
+                select[idx, :] = targets[idx].to_numpy() == val # should output trues or falses into the matrix
+            sub_idx = np.unravel_index(lin_idx, subtracted_response.shape) # unravel from linear to subscript
+            subtracted_response[sub_idx] = np.mean(resp[:, np.all(select, axis=0)], axis=(0, 1)) - np.mean(base[:, np.all(select, axis=0)], axis=(0, 1))
+        return subtracted_response, u_targets
--- generate_data.py
+++ generate_data.py
@@ -4,17 +4,14 @@ import numpy as np
 from Cell import Cell
 import matplotlib.pyplot as plt
 n_trials = 500
-trialwise = np.random.rand(100, 200, n_trials) # 100 cells, 200 frames, 50 trials
+trialwise = np.random.rand(1, 200, n_trials) # 100 cells, 200 frames, 50 trials
 s2p_data = {'info': 3}
+# is_cell = np.load() # this is the vector from suite2p determining if it's a cell or not, we should add this..
 
-cells = list()
-for ii in np.arange(trialwise.shape[0]):
-    cells.append(Cell(neural_data=np.squeeze(trialwise[ii, :, :]), s2p_data=s2p_data))
+cells = Cell(neural_data=np.squeeze(trialwise[0, :, :]), s2p_data=s2p_data, is_cell=is_cell[0], frame_rate=10)
 
-for c in cells:
-    label = np.random.randint(15, size=n_trials)
-    c.add_label(label, 'test')
-
-for c in cells:
-    y, x = c.average_over('test')
-    plt.plot(x, y)
+label = np.random.randint(12, size=n_trials) # eg orientations
+cells.add_label(label, 'test')
+label = np.random.randint(3, size=n_trials) # eg sizes
+cells.add_label(label, 'test2')
+y, x = cells.average_over([0, 5, 5.1, 7], ['test', 'test2'])
