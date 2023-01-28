1a82358f79a436a068634103706df3fb2e248482
Kevin Sit <kevinkamansit@gmail.com>
2023-01-27 16:51:51 -0800

    continued development

--- cellwrapper.py
+++ cellwrapper.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import itertools
 from holofun.s2p import Suite2pData
-
+import copy
 
 def create_cells(data:np.ndarray, s2p:Suite2pData):
     stat = s2p.get_stat_iscell()
@@ -13,73 +13,85 @@ def create_cells(data:np.ndarray, s2p:Suite2pData):
     return cells
 
 class Cell:
-    response_win = []
-
     def __init__(self, neural_data:np.ndarray, s2p_data:dict, frame_rate:int):
         self.s2p_data = s2p_data # this should be a dict, from s2p's stat.npy
         self.neural_data = neural_data
         self.labels = pd.DataFrame()
         self.outputs = pd.DataFrame()
         self.time = np.arange(neural_data.shape[1])/frame_rate
+        self.response_win = []
 
-    def add_label(self, input, label_name=None):
-        input, name = self._check_inputs(input, label_name)
-        self.labels[name] = input
-
-    def add_output(self, input, output_name=None):
-        input, name = self._check_inputs(input, output_name)
-        self.outputs[name] = input
-
-    def _check_inputs(self, input, name):
-        if type(input) is dict:
-            name = list(input.keys())[0]
-            input = list(input.values())[0]
-        else:
-            if name is None:
-                raise TypeError(f"If input is not a dictionary, must include label names")
+    def add_label(self, input, trim=False):
+        input = self._check_inputs(input)
+        if trim:
+            input = input.iloc[range(self.neural_data.shape[0]), :]
         if input.shape[0] != self.neural_data.shape[0]:
             raise IndexError(f"Labels do not have the right number of trials ({self.neural_data.shape[0]})") # trials
-        return input, name
+        self.labels = self._add(input, self.labels)
 
+    def add_output(self, input):
+        input = self._check_inputs(input)
+        self.outputs = self._add(input, self.outputs)
+
+    def _add(self, input, df):
+
+        for k, v in input.items():
+            if k in df.keys():
+                df[k] = v
+            else:
+                df = pd.concat([df, input], axis=1)
+        return df
+
+    def _check_inputs(self, input):
+        if type(input) is not dict:
+            raise TypeError(f"Input needs to be a dictionary, instead it was a {type(input)}")
+        for k, v in input.items():
+            if np.isscalar(v):
+                input[k] = [v]
+        return pd.DataFrame(input)
+
+    def drop_trial(self, trials_to_drop):
+        df = self.copy()
+        # used to drop trials, eg not useful ones etc
+        if trials_to_drop.dtype is np.dtype('bool'):
+            trials_to_drop = np.asarray(np.where(trials_to_drop)[0])
+        if not df.labels.empty: 
+            df.labels.drop(index=trials_to_drop, inplace=True)
+        df.neural_data = np.delete(df.neural_data, trials_to_drop, axis=0)
+        return df
+
+    def calculate_response_window(self):
+        # get the time average here, then collapse into 1 x trials
+        # then concatenate with labels into a dataframe, then you can use that stuff
+        baseline_idx = (self.time > self.response_win[0]) & (self.time < self.response_win[1])
+        response_idx = (self.time > self.response_win[2]) & (self.time < self.response_win[3])
+        base = np.mean(self.neural_data[:, baseline_idx], axis=1)
+        resp = np.mean(self.neural_data[:, response_idx], axis=1)
+        return resp, base
+
+    def average_over(self, select):
+        # theoretically creating a dataframe?
+        if type(select) is not list:
+            select = [select]
+        df = self.labels[select].copy()
+        resp, base = self.calculate_response_window()
+        df['data'] = resp - base
+        m = df.groupby([*select]).mean(numeric_only=True).reset_index()
+        e = df.groupby([*select]).sem(numeric_only=True).reset_index()
+        m['error'] = e['data'] # mapping should be preserved
+        return m
+
+    # Getters and Setters
     @property
     def n_trials(self):
         return self.neural_data.shape[0]
 
     @property
     def n_frames(self):
-        return self.neural_data.shape[1]
+        return self.neural_daters
 
-    @classmethod
-    def set_response_window(cls, input):
-        Cell.response_win = input
-        print(f"Response window set to {Cell.response_win}")
+    def set_response_window(self, input):
+        self.response_win = input
 
-    def average_over(self, input=None):
-        baseline_idx = (self.time > self.response_win[0]) & (self.time < self.response_win[1])
-        response_idx = (self.time > self.response_win[2]) & (self.time < self.response_win[3])
-        base = self.neural_data[:, baseline_idx]
-        resp = self.neural_data[:, response_idx]
-        if input is None: # grand average across conditions
-            return np.mean(resp) - np.mean(base), np.arange(self.neural_data.shape[0])
-
-        # check if input is a list
-        if type(input) is not list:
-            input = [input]
-
-        targets = list()
-        u_targets = list()
-        for i in input:
-            # for all the inputs, get the proper values
-            targets.append(self.labels[i])
-            u_targets.append(sorted(self.labels[i].unique()))
-        subtracted_response = np.zeros([len(x) for x in u_targets])
-        # not the fastest way of doing this... can we think of something faster? idk
-        for lin_idx, combo in enumerate(itertools.product(*u_targets)):
-            # for each potential combination of targets (list product)
-            select = np.zeros((len(targets), self.neural_data.shape[0]))  # this is targets x trials
-            for idx, val in enumerate(combo):
-                # for each target, collect the trues for that value
-                select[idx, :] = targets[idx].to_numpy() == val # should output trues or falses into the matrix
-            sub_idx = np.unravel_index(lin_idx, subtracted_response.shape) # unravel from linear to subscript
-            subtracted_response[sub_idx] = np.mean(resp[np.all(select, axis=0), :], axis=(0, 1)) - np.mean(base[np.all(select, axis=0), :], axis=(0, 1))
-        return subtracted_response, np.squeeze(u_targets)
+    def copy(self):
+        return copy.deepcopy(self)
